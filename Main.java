9badd5f9aae6efe1ac26300c30f0d428898e27b8
repl.it user <replituser@example.com>
2020-10-23 12:08:46 +0000

    Assignment1

--- Main.java
+++ Main.java
@@ -1,5 +1,24 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
-  }
+	int zahl = 0;
+      	for (int i = 0; i <= 200; i++){
+          
+          zahl = zahl + 1;
+          int vorgänger = zahl - 1;
+          int ergebniss = zahl + vorgänger;
+
+          if (zahl % 5 == 0){
+             System.out.println("Zahl ist durch 5 Teilbar");
+          }
+
+          if (zahl % 10 == 9 ){
+            System.out.println("Zahl ist durch 9 Teilbar");
+         }
+
+          if ((zahl + vorgänger) % 3 == 0){
+            System.out.println("Vorgänger " + vorgänger + " addiert mit Zahl " + zahl +" ergibt " + ergebniss + 
+            " ist durch 3 Teilbar");
+          }        
+        }     
+  } 
 }
